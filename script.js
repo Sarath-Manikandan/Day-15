9ed04e8165189eb3547b46ca58766deba2cf32ac
Sarath Manikandan <63185063+Sarath-Manikandan@users.noreply.github.com>
2023-03-18 15:48:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
-(function(){
- var screen=document.querySelector(".screen");
- var buttons=document.querySelectorAll(".btn");
- var equal=document.querySelector(".btn-greya");
- var clear = document.querySelector(".btn-greyb");
- 
+var section = document.createElement('section');
+section.classList.add('calculator');
 
-buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-        let value = e.target.dataset.num;
-        screen.value += value;
-    })
-});
+var form = document.createElement('form');
+section.appendChild(form);
 
-equal.addEventListener("click", function(e){
-    if(screen.value === ""){
-        screen.value=""
-    }else{
-        var answer = eval(screen.value);
-        screen.value = answer;
-    }
-})
+var input = document.createElement('input');
+input.setAttribute('type', 'text');
+input.classList.add('screen');
+form.appendChild(input);
 
-clear.addEventListener("click", function(e){
-    screen.value = "";
-})
-})();
+var div = document.createElement('div');
+div.classList.add('buttons');
+section.appendChild(div);
 
+var buttonLabels = ['*', '/', '-', '+', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+for (const label of buttonLabels) {
+  var button = document.createElement('button');
+  button.setAttribute('type', 'button');
+  button.classList.add('btn');
+  if (label.match(/[0-9,.]/)) {
+    button.classList.add('btn-grey');
+  } else {
+    button.classList.add('btn-yellow');
+  }
+  button.textContent = label;
+  button.dataset.num = label;
+  button.addEventListener('click', function() {
+    input.value += label;
+  });
+  div.appendChild(button);
+}
 
+var equal = document.createElement('button');
+equal.setAttribute('type', 'button');
+equal.classList.add('btn-greya');
+equal.textContent = '=';
+equal.addEventListener('click', calculateResult);
+function calculateResult() {
+  if (input.value === "") {
+    input.value = "";
+  } else {
+    var answer = eval(input.value);
+    input.value = answer;
+  }
+}
+div.appendChild(equal);
+
+var clear = document.createElement('button');
+clear.setAttribute('type', 'button');
+clear.classList.add('btn-greyb');
+clear.textContent = 'C';
+clear.addEventListener('click', clearInput);
+function clearInput() {
+    input.value = "";
+  }
+div.appendChild(clear);
+
+document.body.appendChild(section);
